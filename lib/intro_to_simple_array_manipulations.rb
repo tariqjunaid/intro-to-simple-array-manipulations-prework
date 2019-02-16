4300ccbcf6b6a4a0261a6a59a79893acf097899e
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-02-16 02:21:45 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(ary, str)
+ ary << (str)
+end
+
+def using_unshift(ary, str)
+  ary.unshift(str)
+end
+
+def using_pop(ary)
+  ary.pop
+end
+
+def pop_with_args(ary)
+  ary.pop(2)
+end
+
+def using_shift(ary)
+  ary.shift
+end
+
+def shift_with_args(ary)
+  ary.shift(2)
+end
+
+def using_concat(ary1, ary2)
+  ary1.concat(ary2)
+end
+
+def using_insert(ary, obj)
+  ary.insert(4, obj)
+end
+
+def using_uniq(ary)
+  ary.uniq
+end
+
+def using_flatten(ary)
+  ary.flatten
+end
+
+def using_delete(ary, str)
+  ary.delete(str)
+end
+
+def using_delete_at(ary, int)
+  ary.delete_at(int)
+end
